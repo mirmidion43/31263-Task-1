2e95cbea8e61c72e6a93b891ae3fa80a1c2834cb
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-09-17 16:16:40 +1000

    Top left corner now instantiates at proper rotations

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -33,17 +33,26 @@ public class LevelGenerator : MonoBehaviour
     public GameObject TJunc;
     public GameObject TestTile;
     public GameObject TestCube;
+    public GameObject RInCorner;
+
+    Vector3 rotation1 = new Vector3(0,0,0);
+    Vector3 rotation2 = new Vector3(0,0,90);
+    Vector3 rotation3 = new Vector3(0,0,180);
+    Vector3 rotation4 = new Vector3(0,0,270);
+
 
 
     // Called before Start
     private void Awake() 
     {
-        Debug.Log("Awake Called");
         for (int i = 0; i < 14; i++)
         {
             for (int j = 0; j < 15; j++)
             {
                 makeTile(levelMap[j,i], i, j); 
+                //makeTile(levelMap[j,i], -i, j); 
+                //makeTile(levelMap[j,i], i, -j); 
+                //makeTile(levelMap[j,i], -i, -j); 
             }
         }
 
@@ -80,9 +89,9 @@ public class LevelGenerator : MonoBehaviour
         float d2 = -(-y + 14.5f);
         
         makeDecider(ID, a1,a2);
-        makeDecider(ID, b1,b2);
-        makeDecider(ID, c1,c2);
-        makeDecider(ID, d1,d2);
+        //makeDecider(ID, b1,b2);
+       // makeDecider(ID, c1,c2);
+       // makeDecider(ID, d1,d2);
     }
 
     private void makeDecider(int ID, float x, float y)
@@ -112,41 +121,98 @@ public class LevelGenerator : MonoBehaviour
     //make an Outside Corner
     private void make1(float x, float y)
     {
-        Instantiate(OutCorner, new Vector3(x,y,0), Quaternion.identity);
+        if(x==-13.5 && y==5.5)
+            Instantiate(OutCorner, new Vector3(x,y,0), Quaternion.Euler(rotation2));
+        else if(x==-8.5 && y==5.5)
+            Instantiate(OutCorner, new Vector3(x,y,0), Quaternion.Euler(rotation4));
+        else if(x==-8.5 && y==1.5)
+            Instantiate(OutCorner, new Vector3(x,y,0), Quaternion.Euler(rotation3));
+        else
+            Instantiate(OutCorner, new Vector3(x,y,0), Quaternion.Euler(rotation1));
+        
     }
     //make an Outside Wall
     private void make2(float x, float y)
     {
-        Instantiate(OutWall, new Vector3(x,y,0), Quaternion.identity);
+        if(y==14.5||y==5.5||y==1.5)
+            Instantiate(OutWall, new Vector3(x,y,0), Quaternion.Euler(rotation2));
+        else
+            Instantiate(OutWall, new Vector3(x,y,0), Quaternion.Euler(rotation1));
     }
 
     //make an Inside Corner
     private void make3(float x, float y)
     {
-        Instantiate(InCorner, new Vector3(x,y,0), Quaternion.identity);
+        if((x==-5.5 && y ==5.5)||(x==-5.5 && y==4.5)||(x==-0.5 && y ==7.5))
+            make3R(x,y);
+        else
+            make3N(x,y);  
+    }
+
+    private void make3R(float x, float y)
+    {
+        if(x==-5.5 && y==4.5)
+        Instantiate(RInCorner, new Vector3(x,y,0), Quaternion.Euler(rotation4));
+        else if (x==-0.5 && y==7.5)
+        Instantiate(RInCorner, new Vector3(x,y,0), Quaternion.Euler(rotation3));
+        else
+        Instantiate(RInCorner, new Vector3(x,y,0), Quaternion.Euler(rotation1));
+    }
+
+    private void make3N(float x, float y)
+    {
+        if(y==12.5||y==8.5||y==5.5)
+        {
+            if(x==-8.5||x==-2.5||x==-5.5)
+                Instantiate(InCorner, new Vector3(x,y,0), Quaternion.Euler(rotation4));
+            else 
+            Instantiate(InCorner, new Vector3(x,y,0), Quaternion.Euler(rotation1));
+        }
+
+        else if(y==10.5||y==7.5||y==1.5||y==4.5)
+        {
+            if(x==-11.5||x==-6.5||x==-3.5||x==-0.5)
+                Instantiate(InCorner, new Vector3(x,y,0), Quaternion.Euler(rotation2));
+            else if(x==-8.5||x==-2.5||x==-5.5)
+                Instantiate(InCorner, new Vector3(x,y,0), Quaternion.Euler(rotation3));
+        }
+                   
+        else 
+            Instantiate(InCorner, new Vector3(x,y,0), Quaternion.Euler(rotation1));
     }
 
     //make an Inside Wall
     private void make4(float x, float y)
     {
-        Instantiate(InWall, new Vector3(x,y,0), Quaternion.identity);
+        if(-11.0<x && x<-1.0 && x!=-6.5 && y>2)
+            if(y==11.5||y==6.5||y==3.5||(y==7.5&&x==-5.5)||(y==2.5&&x==-5.5))
+                Instantiate(InWall, new Vector3(x,y,0), Quaternion.Euler(rotation1));
+            else if(y==10.5||y==4.5||y==7.5)
+                Instantiate(InWall, new Vector3(x,y,0), Quaternion.Euler(rotation4));
+            else  
+                Instantiate(InWall, new Vector3(x,y,0), Quaternion.Euler(rotation2));
+        
+        else if(y==8.5 && x==-0.5)
+            Instantiate(InWall, new Vector3(x,y,0), Quaternion.Euler(rotation2));
+        else    
+            Instantiate(InWall, new Vector3(x,y,0), Quaternion.Euler(rotation3));
     }
 
     //make a Standard Pellet
     private void make5(float x, float y)
     {
-        Instantiate(TestTile, new Vector3(x,y,0), Quaternion.identity);
+        Instantiate(TestTile, new Vector3(x,y,0), Quaternion.Euler(rotation1));
     }
 
     //make a Power Pellet
     private void make6(float x, float y)
     {
-        Instantiate(TestCube, new Vector3(x,y,0), Quaternion.identity);
+        Instantiate(TestCube, new Vector3(x,y,0), Quaternion.Euler(rotation1));
     }
 
     //make a T-junction
     private void make7(float x, float y)
     {
-        Instantiate(TJunc, new Vector3(x,y,0), Quaternion.identity);
+        Instantiate(TJunc, new Vector3(x,y,0), Quaternion.Euler(new Vector3(0,180,0)));
     }
 }
